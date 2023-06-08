0806288972236da5f847554aaf749667958dcd5e
Kesh1508 <keshave2000@gmail.com>
2023-06-08 23:38:42 +0530

    Initial commit

--- alu.v
+++ alu.v
@@ -2,17 +2,19 @@ module K_ALU (
   input [31:0] opA,
   input [31:0] opB,
   output enable,
+  output write_enable
   output reg [31:0] result
 
 );
   
   always @ (opA) begin
-   enable = 1; // Addition 
+   enable = 1;  
   end
 
 
   always @ (opB) begin
-   result = opA + opB; // Addition 
+   result = opA + opB; // Addition
+   write_enable = 1; 
   end
 
 endmodule
--- dmem.v
+++ dmem.v
@@ -1,15 +1,16 @@
 module DataMemory (
-  input [31:0] address,
+
   input [31:0] write_data,
   input write_enable,
   output reg [31:0] read_data
 );
   reg [31:0] mem [0:1023]; // Data memory with 1024 locations, each 32 bits wide
-
+  reg [9:0] address = 0;
   always @(address, write_data, write_enable) begin
     if (write_enable)
       mem[address] <= write_data; // Write data to the memory location specified by the address if write_enable is asserted
     read_data <= mem[address]; // Read data from the memory location specified by the address
+    address <= address + 1
   end
 endmodule
 
