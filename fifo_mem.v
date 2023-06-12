595552cef2ba00288d5aaa2cd8168901ce9adc17
keshavek <48319804+kesh1508@users.noreply.github.com>
2023-06-12 15:47:00 +0530

    Update and rename fifo_rdmem.v to fifo_mem.v

--- fifo_mem.v
+++ fifo_mem.v
@@ -1,31 +1,29 @@
-module MemoryToFifo (
+module fifoMem  (
   input wire clk,
   input wire rst,
   input wire read_enable,
+  input wire write_enable,
+  input reg read_data,
   output wire data_out
 );
 
   // Define parameters for the FIFO size
-  parameter DATA_WIDTH = 8;  // Width of the data in bits
+  parameter DATA_WIDTH = 32;  // Width of the data in bits
   parameter FIFO_DEPTH = 16; // Depth of the FIFO in number of elements
 
   // Define FIFO memory array
   reg [DATA_WIDTH-1:0] fifo [0:FIFO_DEPTH-1];
-
-  // Define read and write pointers
-  reg [log2(FIFO_DEPTH)-1:0] read_ptr;
-  reg [log2(FIFO_DEPTH)-1:0] write_ptr;
-
   // Define register to hold the output data
   reg [DATA_WIDTH-1:0] register_out;
-
+  reg [FIFO_DEPTH-1:0] write_ptr;
   // Write logic
   always @(posedge clk) begin
     if (rst) begin
       write_ptr <= 0;
-    end else if (read_enable) begin
+    end 
+    else if (read_enable) begin
+      fifo[write_ptr] <= read_data;  // Replace 'memory_data' with the actual signal from memory
       write_ptr <= write_ptr + 1;
-      fifo[write_ptr] <= memory_data;  // Replace 'memory_data' with the actual signal from memory
     end
   end
 
@@ -35,8 +33,8 @@ module MemoryToFifo (
       read_ptr <= 0;
       register_out <= 0;
     end else if (read_enable) begin
-      read_ptr <= read_ptr + 1;
-      register_out <= fifo[read_ptr];
+       register_out <= fifo[read_ptr];
+       read_ptr <= read_ptr + 1;
     end
   end
 
@@ -44,4 +42,3 @@ module MemoryToFifo (
   assign data_out = register_out;
 
 endmodule
-
