6fa002ff70f5698d197b8802b1cfaa7ae08e00f5
keshavek <48319804+kesh1508@users.noreply.github.com>
2023-06-12 14:46:20 +0530

    Update value_b_Holder.v

--- value_b_Holder.v
+++ value_b_Holder.v
@@ -1,12 +1,12 @@
 module K_value_b_Holder (
-  input wire enable,
+  
   output reg [15:0] value
 );
 
-  always @(posedge clk) begin
-    if (enable) begin
+  
+   initial begin 
       value <= 16'h00FF;
-    end
+    
   end
 
 endmodule
